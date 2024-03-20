9e6e92c1878a906b21fc5495dcc8a7b5591e3592
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2024-03-20 13:04:43 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Khaleb Lisboa on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
